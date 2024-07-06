325eb690f49a5f1f08795b31252383a34e26bb83
Harooonsahil <haroonsahil671@gmail.com>
2024-07-06 23:52:26 +0500

    assignment

---
+++ script.js
@@ -0,0 +1,56 @@
+// TABLE
+/* let tab= prompt("Enter any number");
+ for(i=1;i<=10;i++)
+    {
+        console.log(`${tab} * ${i} = ${tab*i}`)
+    } */
+    
+
+
+    // print the sum of array elements
+     // PART 1
+     /*let arrey=[];
+     for(i=0;i<5;i++)
+        {
+            let nums=prompt("enter number");
+            arrey.push(nums)
+        } 
+        console.log(arrey) */
+
+         // PART 2
+       /* let arrey=[2,3,4,5,6,]
+           sum=0;
+           for(i=0;i<arrey.length;i++)
+           {
+              sum=sum+arrey[i];
+           }
+           console.log(sum); */
+
+          /* let arrey=[];
+     for(i=0;i<5;i++)
+        {
+            let nums=prompt("enter number");
+            arrey.push(nums)
+        } 
+        console.log(arrey)*/
+      
+
+        // factorial
+       /* let num=prompt("enter ay number");
+        let fact=1;
+        for(i=1;i<=num;i++)
+        {
+            fact=fact*i;
+        }
+        console.log(`${fact}`);*/
+    
+          
+    
+
+
+
+
+
+
+
+
